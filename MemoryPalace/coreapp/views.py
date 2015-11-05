d12545111ce59bd41ac959beb3cd597d8a627c66
kaiying <ckykokoko@gmail.com>
2015-11-05 13:56:37 -0500

    add passible_cavas

--- MemoryPalace/coreapp/views.py
+++ MemoryPalace/coreapp/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
+def room(req):
+
+    return render_to_response('room.html',{ 'title': 'room'})
+	
+	
 def index(req):
-    return HttpResponse('<h1>memory palace<h1>')
+
+    return render_to_response('index.html',{ 'title': 'homepage'})
