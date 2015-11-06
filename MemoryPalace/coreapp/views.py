237d88b7a05eedf3da8be2f09df5870639bcc1ef
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-11-05 22:42:32 -0500

    MATERIALIZE made modular

--- MemoryPalace/coreapp/views.py
+++ MemoryPalace/coreapp/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
-def room(req):
-
-    return render_to_response('room.html',{ 'title': 'room'})
-	
-	
 def index(req):
-
-    return render_to_response('index.html',{ 'title': 'homepage'})
+    data = { 'title': 'homepage'}
+    return render_to_response('home.html',data)
