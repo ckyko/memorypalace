64362e33b7e639e444aa05c56080eb125fffc60d
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-11-09 17:11:23 -0500

    functional_test.py and test.py modified

--- MemoryPalace/functional_tests.py
+++ MemoryPalace/functional_tests.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'MemoryPalace' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Amber needs to memorize som chemistry terms and goes to MemoryPalace
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention MemoryPalace
+        self.assertIn('MemoryPalace', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Memory Palace', header_text)
+
+if __name__ == '__main__':
+    unittest.main()
