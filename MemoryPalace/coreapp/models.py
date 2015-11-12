7204b2c43869a9c092d6ef18a97b63e6fa8b7c07
Enan Rahman <enan7890@gmail.com>
2015-11-12 17:30:51 -0500

    database changes

--- MemoryPalace/coreapp/models.py
+++ MemoryPalace/coreapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-	Username = models.CharField(max_length=20)
+	Username = models.CharField(max_length=20,primary_key=True)
 	Email = models.CharField(max_length=200)
 	#EncryptedPassword
 
