f6894661546e28d16dcd5efd6df05a3d146ea9f7
Enan Rahman <enan7890@gmail.com>
2015-11-15 13:59:20 -0500

    pull and edited database

--- MemoryPalace/coreapp/static/js/jav.js
+++ MemoryPalace/coreapp/static/js/jav.js
@@ -1,3 +1,4 @@
+//*** DRAGGING CODE ***
 // target elements with the "draggable" class
 interact('.draggable')
   .draggable({
@@ -23,47 +24,73 @@ interact('.draggable')
         + (Math.sqrt(event.dx * event.dx +
                      event.dy * event.dy)|0) + 'px');
     }
-  })  
-  .resizable({
-    preserveAspectRatio: true,
-    edges: { left: true, right: true, bottom: true, top: true }
   })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
+//*** THE REST OF THE DRAGGING CODE CONTINUES AFTER THE END OF THE RESIZING CODE ***
 
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
+	//*** RESIZING CODE ***
+	  .resizable({
+	    preserveAspectRatio: true,
+	    edges: { left: true, right: true, bottom: true, top: true }
+	  })
+	  .on('resizemove', function (event) {
+	    var target = event.target,
+		x = (parseFloat(target.getAttribute('data-x')) || 0),
+		y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
+	    // update the element's style
+	    target.style.width  = event.rect.width + 'px';
+	    target.style.height = event.rect.height + 'px';
 
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
+	    // translate when resizing from top or left edges
+	    x += event.deltaRect.left;
+	    y += event.deltaRect.top;
 
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
-  });
+	    target.style.webkitTransform = target.style.transform =
+		'translate(' + x + 'px,' + y + 'px)';
 
+	    target.setAttribute('data-x', x);
+	    target.setAttribute('data-y', y);
+	    target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+	  });
+	//*** END RESIZING CODE ***
+
+//*** CONTINUE DRAGGING CODE ***
   function dragMoveListener (event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+	// translate the element
+	target.style.webkitTransform =
+	target.style.transform =
+	  'translate(' + x + 'px, ' + y + 'px)';
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
+	// update the posiion attributes
+	target.setAttribute('data-x', x);
+	target.setAttribute('data-y', y);
+}
 
   // this is used later in the resizing and gesture demos
   window.dragMoveListener = dragMoveListener;
+
+//*** FOR THE HOME PAGE SLIDER ***
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+  
+//*** FOR HTE ROOM UPLOAD IMAGES ***
+$(function(){
+   $("#file").change(function(e){
+		 var file = e.target.files||e.dataTransfer.files;
+		 
+		 if(file){
+			 var reader = new FileReader();
+			 reader.onload=function(){
+					// append the "<img class='draggable' src=... />" to roombg
+					$("<img class='draggable' src='"+this.result+"'/>").appendTo("#roombg");
+			 }
+			reader.readAsDataURL(file[0]);
+		}
+  });
+})
