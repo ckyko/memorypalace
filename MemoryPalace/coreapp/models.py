646624e907d48772b6d7f22f9bf4305ac835e296
Enan Rahman <enan7890@gmail.com>
2015-11-17 17:12:40 -0500

    edited static implementation

---
+++ MemoryPalace/coreapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Users(models.Model):
+    Username = models.CharField(max_length=20,primary_key=True)
+    Email = models.CharField(max_length=200)
+    #EncryptedPassword
+
+class UserPalaces(models.Model):
+    #Username = models.ForeignKey(Users)
+    PalaceName = models.CharField(max_length=200)
+    BackgroundImage = models.CharField(max_length=200)
+
+class PalaceObjects(models.Model):
+    #Username = models.ForeignKey(Users)
+    #  #PalaceName = models.ForeignKey(UserPalaces)
+    ObjectName= models.CharField(max_length=200)
+    #FileLocation(Coordinates on page)
+    Description = models.CharField(max_length=200)
+    ObjectImage = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.ObjectName
