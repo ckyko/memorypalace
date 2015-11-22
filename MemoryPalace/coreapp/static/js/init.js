53c3569622bc7dbf75009492783ad358b4aa6a86
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-11-22 16:27:29 -0500

    addded modals to memory palace for more objects

--- MemoryPalace/coreapp/static/js/init.js
+++ MemoryPalace/coreapp/static/js/init.js
@@ -6,5 +6,13 @@
     $(document).ready(function(){
     $('ul.tabs').tabs();
     });
+    $(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+    }
+  );
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
