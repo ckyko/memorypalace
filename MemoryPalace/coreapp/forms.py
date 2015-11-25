e441e6b576177e82cc20339ca0dcc1e08d25b759
kaiying <ckykokoko@gmail.com>
2015-11-24 23:28:11 -0500

    edit forms and urls

--- MemoryPalace/coreapp/forms.py
+++ MemoryPalace/coreapp/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 from django.forms import  ModelForm
-from .models import PalaceRoom
+from .models import PalaceRoom, UserPalace
 # from django.contrib.auth.models import User
 
-class CreatePalaceForm(forms.Form):
-    palaceName = forms.CharField()
-    numOfRooms = forms.IntegerField()
-    public = forms.BooleanField(initial=False)
+# class CreatePalaceForm(forms.Form):
+#     palaceName = forms.CharField()
+#     numOfRooms = forms.IntegerField()
+#     public = forms.BooleanField(initial=False)
+
+class CreatePalaceForm(ModelForm):
+    class Meta:
+        model = UserPalace
+        fields = ['palaceName', 'numOfRooms', 'public']
 
 # class CreateRoomForm(forms.Form):
 #     roomName = forms.CharField()
