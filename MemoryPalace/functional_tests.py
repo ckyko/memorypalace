9d70e6e1beb46a13dfaf97185f8041adb967a8d2
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-12-03 17:52:08 -0500

    functional test with login|register modal  working

--- MemoryPalace/functional_tests.py
+++ MemoryPalace/functional_tests.py
@@ -22,8 +22,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('Welcome!', header_text)
 
     def test_2_register(self):
-        self.browser.get('http://localhost:8000/register')
+        self.browser.get('http://localhost:8000')
         self.assertIn('MemoryPalace', self.browser.title)
+        element = self.browser.find_element_by_id("header_modal_L|R")
+        element.click()
+        element = self.browser.find_element_by_id("tab_modal_register")
+        element.click()
         element = self.browser.find_element_by_id("id_username")
         element.send_keys("testuser")
         element = self.browser.find_element_by_id("email")
@@ -32,19 +36,22 @@ class NewVisitorTest(unittest.TestCase):
         element.send_keys("testuser")
         element = self.browser.find_element_by_id("id_password2")
         element.send_keys("testuser")
-        self.browser.find_element_by_id("register_submit").click()
+        self.browser.find_element_by_id("register_submit").submit()
         self.assertEquals(self.browser.current_url, "http://localhost:8000/" );
 
     def test_3_login(self):
-        self.browser.get('http://localhost:8000/login')
+        self.browser.get('http://localhost:8000')
         self.assertIn('MemoryPalace', self.browser.title)
+        element = self.browser.find_element_by_id("header_modal_L|R")
+        element.click()
+        element = self.browser.find_element_by_id("tab_modal_login")
+        element.click()
         element = self.browser.find_element_by_id("username")
         element.send_keys("testuser")
         element = self.browser.find_element_by_id("password")
         element.send_keys("testuser")
-        self.browser.find_element_by_id("login_submit").click()
+        self.browser.find_element_by_id("login_submit").submit()
         self.assertEquals(self.browser.current_url, "http://localhost:8000/" );
-
 
 if __name__ == '__main__':
     unittest.main()
