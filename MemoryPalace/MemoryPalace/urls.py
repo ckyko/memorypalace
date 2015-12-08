392b3603c061f8990249c22a38fde6d8af05156e
kaiying <ckykokoko@gmail.com>
2015-12-08 02:01:15 -0500

    add name for urls

--- MemoryPalace/MemoryPalace/urls.py
+++ MemoryPalace/MemoryPalace/urls.py
@@ -21,16 +21,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'coreapp.views.index'),
-    url(r'^about/', 'coreapp.views.about'),
-    url(r'^contact/', 'coreapp.views.contact'),
-    url(r'.*MemoryPalace', 'coreapp.views.MemoryPalace'),
-    url(r'.*login/', 'coreapp.views.log_in'),
-    url(r'.*register/', 'coreapp.views.register'),
-    url(r'.*createPalace/', 'coreapp.views.createPalace'),
-    url(r'^palace_library/', 'coreapp.views.palace_library'),
-    url(r'^logout/', 'coreapp.views.log_out'),
+    url(r'^$', 'coreapp.views.index', name='index'),
+    url(r'^about/', 'coreapp.views.about', name='about'),
+    url(r'^contact/', 'coreapp.views.contact', name = 'contact'),
+    url(r'.*MemoryPalace', 'coreapp.views.MemoryPalace', name='MemoryPalace'),
+    url(r'.*login/', 'coreapp.views.log_in', name='login'),
+    url(r'.*register/', 'coreapp.views.register', name='register'),
+    url(r'.*createPalace/', 'coreapp.views.createPalace', name='createPalace'),
+    url(r'^palace_library/', 'coreapp.views.palace_library', name='palace_library'),
+    url(r'^logout/', 'coreapp.views.log_out', name='logout'),
     url(r'^testing/', 'coreapp.views.testing'),
-    url(r'^createRoom/', 'coreapp.views.createRoom'),
+    url(r'^createRoom/', 'coreapp.views.createRoom', name='createRoom'),
+    url(r'^upload_image/', 'coreapp.views.upload_image'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
