84b6ffcec1dda84c6b7c91fad122ae1cfc7d3536
Enan Rahman <enan7890@gmail.com>
2015-12-08 17:10:47 -0500

    restful example up for coreapp

--- MemoryPalace/coreapp/models.py
+++ MemoryPalace/coreapp/models.py
@@ -35,13 +35,14 @@ Contains the rooms in the Palace
 """
 class PalaceRoom(models.Model):
     """
+    user: The foreign key to connect it to a user
     userPalace: The foriegn key to the Palace the room belongs in
     roomName: The name of the room coupled with the Palace so that they are
     named uniqely for their palace
     backgroundImage: The background Image of the room
     """
     user = models.ForeignKey(User, null=True)
-    userPalace = models.ForeignKey('UserPalace', null=True)
+    userPalace = models.ForeignKey(UserPalace, null=True)
     roomName = models.CharField(max_length=200, unique=True)
     backgroundImage = models.ImageField(upload_to='./static/images', default='./static/images/room.jpg')
 
@@ -49,7 +50,7 @@ class PalaceRoom(models.Model):
         return self.roomName
 
     class Meta:
-        unique_together = (("userPalace", "roomName"),)
+        unique_together = (("user", "userPalace", "roomName"),)
 
 """
 Palace Object Table
@@ -57,15 +58,17 @@ Contains the objects inside a room
 """
 class PalaceObject(models.Model):
     """
+    user: The foreign key to connect it to a user
+    userPalace: The foriegn key to the Palace the room belongs in
     palaceRoom: The foriegn key that gives you the room the object belongs in.
     objectName: The name of the object unique to the room it is inside
     objectImage: The image used for the object
-    x: The x coordinate of the object in the room
-    y: The y coordinate of the object in the room
+    position_x: The x coordinate of the object in the room
+    position_y: The y coordinate of the object in the room
     """
     user = models.ForeignKey(User, null=True)
     userPalace = models.ForeignKey('UserPalace', null=True)
-    palaceRoom = models.ForeignKey('PalaceRoom', null=True)
+    roomName = models.ForeignKey('PalaceRoom', null=True)
     # objectName= models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200)
     objectImage = models.ImageField(upload_to='./coreapp/static/images'),
@@ -80,4 +83,4 @@ class PalaceObject(models.Model):
         return self.description
 
     class Meta:
-        unique_together = (("palaceRoom", "description"),)
+        unique_together = (("user", "userPalace", "roomName", "description"),)
