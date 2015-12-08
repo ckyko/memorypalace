4d19e0c21e56583d9761631f9b68a823f96af5e2
Enan Rahman <enan7890@gmail.com>
2015-12-08 18:07:49 -0500

    fixed changes

--- MemoryPalace/MemoryPalace/urls.py
+++ MemoryPalace/MemoryPalace/urls.py
@@ -52,12 +52,10 @@ urlpatterns = [
     url(r'.*createPalace/', 'coreapp.views.createPalace'),
     url(r'^palace_library/', 'coreapp.views.palace_library'),
     url(r'^logout/', 'coreapp.views.log_out'),
-    url(r'^testing/', 'coreapp.views.testing'),
     url(r'^createRoom/', 'coreapp.views.createRoom'),
     #url(r'^api', include(router.urls)),
     url(r'^snippets/$', 'coreapp.views.snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'coreapp.views.snippet_detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^some', include('test_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
