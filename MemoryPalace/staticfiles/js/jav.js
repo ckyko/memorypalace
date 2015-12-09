4695ab9df4827ddc48e7e8a8fc0e0455f749b020
kaiying <ckykokoko@gmail.com>
2015-12-09 02:29:00 -0500

    try add object by ajax

---
+++ MemoryPalace/staticfiles/js/jav.js
@@ -0,0 +1,96 @@
+//*** DRAGGING CODE ***
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(event.dx * event.dx +
+                     event.dy * event.dy)|0) + 'px');
+    }
+  })
+//*** THE REST OF THE DRAGGING CODE CONTINUES AFTER THE END OF THE RESIZING CODE ***
+
+	//*** RESIZING CODE ***
+	  .resizable({
+	    preserveAspectRatio: true,
+	    edges: { left: true, right: true, bottom: true, top: true }
+	  })
+	  .on('resizemove', function (event) {
+	    var target = event.target,
+		x = (parseFloat(target.getAttribute('data-x')) || 0),
+		y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+	    // update the element's style
+	    target.style.width  = event.rect.width + 'px';
+	    target.style.height = event.rect.height + 'px';
+
+	    // translate when resizing from top or left edges
+	    x += event.deltaRect.left;
+	    y += event.deltaRect.top;
+
+	    target.style.webkitTransform = target.style.transform =
+		'translate(' + x + 'px,' + y + 'px)';
+
+	    target.setAttribute('data-x', x);
+	    target.setAttribute('data-y', y);
+	    target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+	  });
+	//*** END RESIZING CODE ***
+
+//*** CONTINUE DRAGGING CODE ***
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+	// translate the element
+	target.style.webkitTransform =
+	target.style.transform =
+	  'translate(' + x + 'px, ' + y + 'px)';
+
+	// update the posiion attributes
+	target.setAttribute('data-x', x);
+	target.setAttribute('data-y', y);
+}
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+//*** FOR THE HOME PAGE SLIDER ***
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+  
+//*** FOR HTE ROOM UPLOAD IMAGES ***
+$(function(){
+   $("#file").change(function(e){
+		 var file = e.target.files||e.dataTransfer.files;
+		 
+		 if(file){
+			 var reader = new FileReader();
+			 reader.onload=function(){
+					// append the "<img class='draggable' src=... />" to roombg
+					$("<img class='draggable' src='"+this.result+"'/>").appendTo("#roombg");
+			 }
+			reader.readAsDataURL(file[0]);
+		}
+  });
+})
