35f37a1c7a36e8ee6286cf9d41caef43a3f95866
Enan Rahman <enan7890@gmail.com>
2015-12-10 15:02:54 -0500

    testing serializers

--- MemoryPalace/coreapp/serializers.py
+++ MemoryPalace/coreapp/serializers.py
@@ -16,5 +16,16 @@ class PalaceObjectSerializer(serializers.ModelSerializer):
         fields = ('user', 'userPalace', 'palaceRoom', 'description',
                   'objectImage','width', 'height', 'position_x', 'position_y')
 
+        def create(self, validated_data):
+            return PalaceObject.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            instance.position_X = validated_data.get('position_X', instance.position_X)
+            instance.position_Y = validated_data.get('position_Y', instance.position_Y)
+            instance.width = validated_data.get('width', instance.width)
+            instance.height = validated_data.get('height', instance.height)
+            instance.save()
+            return instance
+
 
 
