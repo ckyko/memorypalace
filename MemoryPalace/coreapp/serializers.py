19989bc9c44467b2ae8c5d64cd39098731c391f8
kaiying <ckykokoko@gmail.com>
2015-12-10 14:04:35 -0500

    fixed width, add doc string for function

---
+++ MemoryPalace/coreapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from coreapp.models import PalaceObject, PalaceRoom, UserPalace
+from django.contrib.auth.models import User
+
+
+
+"""
+Serializer of the the Palace Objects
+"""
+class PalaceObjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PalaceObject
+        fields = ('user', 'userPalace', 'palaceRoom', 'description',
+                  'objectImage','width', 'height', 'position_x', 'position_y')
+
+
+
