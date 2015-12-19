3f4197868c4280ab647aac4a2a55626c7774cbff
sgupta04 <sgupta04@citymail.cuny.edu>
2015-12-19 01:43:53 -0500

    merge fixed

--- MemoryPalace/coreapp/serializers.py
+++ MemoryPalace/coreapp/serializers.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 # Serializer of the the Palace Objects
 
 '''
+"""
 This is the only serializer needed considering we just need the
 palace objects to dyanamically update.
-'''
+"""
 class PalaceObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = PalaceObject
