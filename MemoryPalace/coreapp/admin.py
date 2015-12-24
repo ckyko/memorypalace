53386c53786f04193269411e4aee53685dad904d
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-12-24 18:02:22 -0500

    Enan's merge

---
+++ MemoryPalace/coreapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(UserPalace)
+admin.site.register(PalaceRoom)
+admin.site.register(PalaceObject)
+admin.site.register(RoomObject)
---
+++ MemoryPalace/coreapp/tests/__init__.py
@@ -0,0 +1,7 @@
+'''
+import all the tests and put them here.
+test should begin with test_""
+'''
+from test_models import *
+from test_views import *
+from test_functionality import *
