e40a070eca52c83b079cae72c34ba2ebca257c55
kaiying <ckykokoko@gmail.com>
2015-12-24 13:21:13 -0500

    add ajax to resize

--- MemoryPalace/coreapp/static/js/jav.js
+++ MemoryPalace/coreapp/static/js/jav.js
@@ -31,7 +31,7 @@ interact('.draggable')
 
       $.get("/update/",{'id': id, 'title': title, 'position_x': position_x,
       'position_y':position_y, 'height':height, 'width':width }, function(ret){
-//            alert("success...");
+
         })
 
 
@@ -59,7 +59,20 @@ interact('.draggable')
 	//*** RESIZING CODE ***
 	  .resizable({
 	    preserveAspectRatio: true,
-	    edges: { left: true, right: true, bottom: true, top: true }
+	    edges: { left: true, right: true, bottom: true, top: true },
+	    onend: function(event){
+             var target = event.target,
+             id = target.getAttribute('id'),
+             position_x = target.getAttribute('data-x'),
+             position_y = target.getAttribute('data-y'),
+             title = target.getAttribute('title');
+             var height = $('#'+id).css('height');
+             var width = $('#'+id).css('width');
+
+          $.get("/update/",{'id': id, 'title': title, 'position_x': position_x,
+          'position_y':position_y, 'height':height, 'width':width }, function(ret){
+            })
+	    }
 	  })
 	  .on('resizemove', function (event) {
 	    var target = event.target,
