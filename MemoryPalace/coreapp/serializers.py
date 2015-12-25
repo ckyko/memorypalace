e13be341adccd8c7708a86e44bc2d9750c7e90c5
Enan Rahman <enan7890@gmail.com>
2015-12-25 11:31:39 -0500

    serializer plint increased

--- MemoryPalace/coreapp/serializers.py
+++ MemoryPalace/coreapp/serializers.py
@@ -1,26 +1,35 @@
-from rest_framework import serializers
+"""
+Serializer
+Puts the objects in serial to save them
+"""
 from coreapp.models import PalaceObject, PalaceRoom, UserPalace
+from rest_framework import serializers
 from django.contrib.auth.models import User
 
-# Serializer of the the Palace Objects
 
-"""
-This is the only serializer needed considering we just need the
-palace objects to dyanamically update.
-"""
 class PalaceObjectSerializer(serializers.ModelSerializer):
     """
-    Serializes PalaceObjects to easily save them
+    This is the only serializer needed considering we just need the
+    palace objects to dyanamically update.
     """
     class Meta:
+        """
+        Select a model
+        """
         model = PalaceObject
         fields = ('user', 'userPalace', 'palaceRoom', 'description',
                   'objectImage', 'width', 'height', 'position_x', 'position_y')
 
         def create(self, validated_data):
+            """
+            Make a new PalaceObject for the database
+            """
             return PalaceObject.objects.create(**validated_data)
 
         def update(self, instance, validated_data):
+            """
+            Update PalaceObject
+            """
             instance.position_X = validated_data.get('position_X', instance.position_X)
             instance.position_Y = validated_data.get('position_Y', instance.position_Y)
             instance.width = validated_data.get('width', instance.width)
