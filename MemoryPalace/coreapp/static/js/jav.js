36bf27045f189dc23b108c4d0951d5bb95e42078
Satya Gupta <sgupta04@citymail.cuny.edu>
2015-12-24 22:32:04 -0500

    temporary downgrade from modal caption to prompt caption

--- MemoryPalace/coreapp/static/js/jav.js
+++ MemoryPalace/coreapp/static/js/jav.js
@@ -181,21 +181,8 @@ $(document).ready(function(){
 
     //Add a caption to the image.
     $(document).on('dblclick', '.draggable', function(e) {
-        alert(e.target.id)
-        $('#modal_caption').openModal();
-        $('#modal_caption_done').click(function(){
-        var caption = $("#modal_caption_id").val();
-        //var caption = prompt("Enter a caption for this image.");
+        var caption = prompt("Enter a caption for this image.");
         $('#'+e.target.id).attr('title', caption);
-        });
-
-	/*Materialize tooltip. Having some trouble with this still.
-
-	$(this).attr('class', 'btn tooltipped');
-	$(this).attr('data-position', 'bottom');
-	$(this).attr('data-delay', '50');
-	$(this).attr('data-tooltip', caption);
-	*/
     });
 
     $('#roombg').on('click', function(e) {
