ab9e0999f26f3c01c842dea00a1543a288b1c8ad
kaiying <ckykokoko@gmail.com>
2015-12-26 21:52:08 -0500

    adding html for sending email in contact

---
+++ MemoryPalace/coreapp/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.forms import ModelForm
+from .models import PalaceRoom, UserPalace
+# from django.contrib.auth.models import User
+
+# class CreatePalaceForm(forms.Form):
+#     palaceName = forms.CharField()
+#     numOfRooms = forms.IntegerField()
+#     public = forms.BooleanField(initial=False)
+
+class CreatePalaceForm(ModelForm):
+    class Meta:
+        model = UserPalace
+        fields = ['palaceName']
+
+# class CreateRoomForm(forms.Form):
+#     roomName = forms.CharField()
+#     backgroundImage = forms.ImageField()
+class CreateRoomForm(ModelForm):
+    class Meta:
+        model = PalaceRoom
+        fields = ['roomName', 'backgroundImage']
+
+class UploadImageForm(forms.Form):
+    objectImage = forms.ImageField(widget=forms.FileInput(attrs={
+                  'class':'upload_image_object', 'accept':'image/*',
+                  'style': '  visibility: hidden; width: 1px; height: 1px;'}))
---
+++ MemoryPalace/coreapp/tests/test_models.py
@@ -0,0 +1,69 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from coreapp.models import UserPalace, PalaceRoom, PalaceObject, RoomObject
+from django.contrib.auth.models import User
+
+
+class UserPalaceDataBaseTestCase(TestCase):
+    """
+    Tests wheather the dastabase adds the models as its supposed to
+    """
+    def setUp(self):
+    	"""
+        Inputs tests data for the test
+        :return:
+        """
+        user = User.objects.create_user(username="testing",
+                                        password="password")
+        user_palace = UserPalace.objects.create(user=user,
+                                                palaceName='testing palace')
+        palace_room = PalaceRoom.objects.create(userPalace=user_palace,
+                                                roomName="testing room")
+        palace_object = PalaceObject.objects.create(userPalace=user_palace,
+                                                    description="testing palace object")
+        RoomObject.objects.create(palaceRoom=palace_room, palaceObject=palace_object)
+
+    def test_palace_room_object_create_in_database_or_not(self):
+        """
+        Testing object creation
+        :return:
+        """
+        user = User.objects.get(username="testing")
+        self.assertTrue(user is not None, msg="user not create.")
+        user_palace = UserPalace.objects.get(user=user)
+        self.assertTrue(user_palace is not None, msg="user_palace not create.")
+        user_room = PalaceRoom.objects.get(userPalace=user_palace)
+        self.assertTrue(user_room is not None, msg="user_room not create.")
+        palace_object = PalaceObject.objects.get(userPalace=user_palace)
+        self.assertTrue(palace_object is not None,
+                        msg="palace_object not create.")
+        room_object = RoomObject.objects.get(palaceRoom=user_room)
+        self.assertTrue(room_object is not None,
+                        msg="palace_object not create.")
+
+    def test_palaceName(self):
+        """
+        Test creating a memory palace
+        :return:
+        """
+        user_palace = UserPalace.objects.get(palaceName='testing palace')
+        self.assertTrue(user_palace is not None,
+                        msg="palace name is create not correct")
+        self.assertEqual(user_palace.__unicode__(), user_palace.palaceName)
+
+    def test_roomName(self):
+        """
+        Test if the Room name
+        :return:
+        """
+        palace_room = PalaceRoom.objects.get(roomName="testing room")
+        self.assertTrue(palace_room is not None,
+                        msg="room name is create not correct")
+        self.assertEqual(palace_room.__unicode__(), palace_room.roomName)
+
+    def tearDown(self):
+        """
+        deletes new data from test
+        :return:
+        """
+        del self
